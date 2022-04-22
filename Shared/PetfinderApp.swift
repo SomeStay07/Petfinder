34bf80204ae83d88adc14ab0c5c8d6154727e2b6
tcheberda <tcheberda@vivid.money>
2022-04-22 17:06:33 +0300

    Initial Commit

---
+++ Shared/PetfinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetfinderApp.swift
+//  Shared
+//
+//  Created by Timur Cheberda on 22.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct PetfinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
